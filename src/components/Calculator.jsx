60835e722ae72dc1a36e07f62b1f179293c48e51
Ksenia Danniker <kseniadanniker@gmail.com>
2019-03-24 18:34:21 +0100

    added skeleton

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Calculator</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
---
+++ src/components/Quote.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Quote extends Component {
+  render() {
+    return (
+      <div className="quote">
+        <h1>Quote</h1>
+      </div>
+    );
+  }
+}
+
+export default Quote;
